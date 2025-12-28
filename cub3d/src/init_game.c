06478dcd2d5820d5b0a616fbe761779e5f0dcd4b
aysesude <camiaysesude@gmail.com>
2025-12-28 13:14:41 +0300

    plane_y updated

--- cub3d/src/init_game.c
+++ cub3d/src/init_game.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_game.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raktas <raktas@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*   By: aycami <aycami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 03:11:53 by aycami            #+#    #+#             */
-/*   Updated: 2025/12/28 12:28:43 by raktas           ###   ########.fr       */
+/*   Updated: 2025/12/28 13:13:47 by aycami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static t_player	*init_player(void)
 	player->dir_x = -1;
 	player->dir_y = 0;
 	player->plane_x = 0;
-	player->plane_y = 0.66;
+	player->plane_y = 0.65;
 	return (player);
 }
 
